b0b44e44e5ada57655b5afb4cc0e0c9f75feaa0d
unknown <baudinopierreolivier@gmail.com>
2023-03-02 10:09:23 +0100

    exo 1 add

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # python_ibm_day2
+
+## part 1 (lvl basic)
+
+### found max in using if /else for 3 numbers
+
+###
+
+## part 2 (lvl 2)
---
+++ index.py
@@ -0,0 +1,21 @@
+# Lvl 1
+# ------------------
+# found max in using if /else for 3 numbers
+
+a = 40
+b = 20
+c = 30
+
+max = 0
+
+if a > b and a > c:
+    max = a
+elif b > a and b > c:
+    max = b
+else:
+    max = c
+
+print(max)
+
+# -----------------------
+#
